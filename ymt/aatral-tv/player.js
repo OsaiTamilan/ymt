5f35bcd6d2a2d50e80f532a6ffa3d842b8abf782
OsaiTamilan <95853906+OsaiTamilan@users.noreply.github.com>
2025-03-06 11:53:11 +0530

    Delete ymt/aatral-tv directory

--- ymt/aatral-tv/player.js
+++
@@ -1,8 +0,0 @@
-// This file is required by aatral-tv.html
-import { VideoPlayer } from './js/VideoPlayer.js';
-
-// Initialize the player when the DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-  // Create a new instance of the VideoPlayer
-  const player = new VideoPlayer();
-});
